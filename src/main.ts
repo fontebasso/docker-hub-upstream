8c807510edcbe2cdaa1ada7f8175e969f329e5d5
Samuel Fontebasso <samuel.fonte@agenciasonora.com.br>
2023-02-23 16:16:05 -0300

    fix style and shell exec on commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
-import {exec} from 'child_process'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as shell from '@actions/exec'
 import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
@@ -21,10 +21,7 @@ async function run(): Promise<void> {
         dockerHubImage.updated_at
       )
       const branchName = `update-docker-image-versions-${Date.now()}`
-      await setupGitUser()
-      await createNewBranch(branchName)
-      await commitChanges()
-      await pushChanges(branchName)
+      await commitChanges(branchName)
       const pullRequest = await createPullRequest(branchName)
       await mergePullRequest(pullRequest)
       await deleteBranch(branchName)
@@ -39,74 +36,73 @@ async function run(): Promise<void> {
   }
 }
 
-async function setupGitUser(): Promise<void> {
-  exec(
-    `git config --global user.email 41898282+github-actions[bot]@users.noreply.github.com"`,
-    (error, stdout, stderr) => {
-      if (error) {
-        core.setFailed(error.message)
-      }
-      if (stderr) {
-        core.setFailed(stderr)
-      }
-    }
+async function checkImageDockerName(image: string): Promise<boolean> {
+  const regex = new RegExp(
+    /^([a-z0-9]+[._-]?)+[a-z0-9]+\/([a-z0-9]+[._-]?)+[a-z0-9]+$/
   )
-
-  exec(
-    `git config --global user.name "github-actions[bot]"`,
-    (error, stdout, stderr) => {
-      if (error) {
-        core.setFailed(error.message)
-      }
-      if (stderr) {
-        core.setFailed(stderr)
-      }
-    }
-  )
+  return regex.test(image)
 }
 
-async function createNewBranch(branchName: string): Promise<void> {
-  exec(`git checkout -b ${branchName}`, (error, stdout, stderr) => {
-    if (error) {
-      core.setFailed(error.message)
-    }
-    if (stderr) {
-      core.setFailed(stderr)
-    }
-  })
+async function transformImageNameOfficial(image: string): Promise<string> {
+  const [owner, name] = image.split('/')
+  if (owner === '_') {
+    return `library/${name}`
+  }
+  return image
 }
 
-async function commitChanges(): Promise<void> {
-  exec(`git add docker-image-versions.json`, (error, stdout, stderr) => {
-    if (error) {
-      core.setFailed(error.message)
+async function getLocalImage(
+  image: string
+): Promise<{tag: any; updated_at: any}> {
+  if (fs.existsSync('docker-image-versions.json')) {
+    const content = fs.readFileSync('docker-image-versions.json', 'utf8')
+    const versions = JSON.parse(content)
+    if (versions[image]) {
+      return {tag: versions[image].tag, updated_at: versions[image].updated_at}
     }
-    if (stderr) {
-      core.setFailed(stderr)
+  }
+  return {tag: null, updated_at: null}
+}
+
+async function updateLocalImage(
+  image: string,
+  tag: string,
+  updated_at: string
+): Promise<void> {
+  let versions = {'${image}': {tag, updated_at}}
+
+  if (fs.existsSync('docker-image-versions.json')) {
+    const content = fs.readFileSync('docker-image-versions.json', 'utf8')
+    versions = JSON.parse(content)
+    // @ts-ignore
+    versions[image] = {
+      tag,
+      updated_at
     }
-  })
-  exec(
-    `git commit -m "Update docker image versions"`,
-    (error, stdout, stderr) => {
-      if (error) {
-        core.setFailed(error.message)
-      }
-      if (stderr) {
-        core.setFailed(stderr)
-      }
-    }
+  }
+
+  fs.writeFileSync('docker-image-versions.json', JSON.stringify(versions))
+}
+
+async function getDockerHubVersion(
+  image: string,
+  tag: string
+): Promise<{tag: string; updated_at: string}> {
+  const url = `https://hub.docker.com/v2/repositories/${image}/tags/${tag}`
+  const response = await fetch(url)
+  const json = (await response.json()) as any
+  return {tag: json.name, updated_at: json.last_updated}
+}
+
+async function commitChanges(branchName: string): Promise<void> {
+  await shell.exec('git config --global user.name "github-actions[bot]"')
+  await shell.exec(
+    'git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"'
   )
-}
-
-async function pushChanges(branchName: string): Promise<void> {
-  exec(`git push origin ${branchName}`, (error, stdout, stderr) => {
-    if (error) {
-      core.setFailed(error.message)
-    }
-    if (stderr) {
-      core.setFailed(stderr)
-    }
-  })
+  await shell.exec(`git checkout -b ${branchName}`)
+  await shell.exec('git add docker-image-versions.json')
+  await shell.exec(`git commit -m "Update docker image versions"`)
+  await shell.exec(`git push origin ${branchName}`)
 }
 
 async function createPullRequest(branchName: string): Promise<number> {
@@ -173,64 +169,6 @@ async function publishRelease(version: string): Promise<void> {
     name: version,
     body: `Release ${version}`
   })
-}
-
-async function checkImageDockerName(image: string): Promise<boolean> {
-  const regex = new RegExp(
-    /^([a-z0-9]+[._-]?)+[a-z0-9]+\/([a-z0-9]+[._-]?)+[a-z0-9]+$/
-  )
-  return regex.test(image)
-}
-
-async function transformImageNameOfficial(image: string): Promise<string> {
-  const [owner, name] = image.split('/')
-  if (owner === '_') {
-    return `library/${name}`
-  }
-  return image
-}
-
-async function getLocalImage(
-  image: string
-): Promise<{tag: any; updated_at: any}> {
-  if (fs.existsSync('docker-image-versions.json')) {
-    const content = fs.readFileSync('docker-image-versions.json', 'utf8')
-    const versions = JSON.parse(content)
-    if (versions[image]) {
-      return {tag: versions[image].tag, updated_at: versions[image].updated_at}
-    }
-  }
-  return {tag: null, updated_at: null}
-}
-
-async function updateLocalImage(
-  image: string,
-  tag: string,
-  updated_at: string
-): Promise<void> {
-  let versions = {'${image}': {tag, updated_at}}
-
-  if (fs.existsSync('docker-image-versions.json')) {
-    const content = fs.readFileSync('docker-image-versions.json', 'utf8')
-    versions = JSON.parse(content)
-    // @ts-ignore
-    versions[image] = {
-      tag,
-      updated_at
-    }
-  }
-
-  fs.writeFileSync('docker-image-versions.json', JSON.stringify(versions))
-}
-
-async function getDockerHubVersion(
-  image: string,
-  tag: string
-): Promise<{tag: string; updated_at: string}> {
-  const url = `https://hub.docker.com/v2/repositories/${image}/tags/${tag}`
-  const response = await fetch(url)
-  const json = (await response.json()) as any
-  return {tag: json.name, updated_at: json.last_updated}
 }
 
 run()
